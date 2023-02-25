b9ea2aa37204ca511ec9b2917d6621b85db1a664
wwwouaiebe <github.christian@guyette.be>
2023-02-25 19:50:58 +0100

    Modified error pages

--- dist/RAZARA4anthisnes/_public.php
+++ dist/RAZARA4anthisnes/_public.php
@@ -1,30 +1,5 @@
 <?php
 # empêcher l'exécution du fichier en dehors de Dotclear
 if (!defined('DC_RC_PATH')) {return;}
-
 l10n::set(dirname(__FILE__).'/locales/'.dcCore::app()->lang.'/public');
-
-dcCore::app()->url->register('p401','p401','^p401/(.+)$',array('myURLHandlers','p401'));
-dcCore::app()->url->register('p403','p403','^p403/(.+)$',array('myURLHandlers','p403'));
-
-class myURLHandlers extends dcUrlHandlers
-{
-	protected static function p401()
-	{
-		header('Content-Type: text/html; charset=UTF-8');
-		http::head(401,'Unauthorized');
-		dcCore::app()->url->type = '401';
-		echo dcCore::app()->tpl->getData('401.html');
-		exit;
-	}
-	protected static function p403()
-	{
-		header('Content-Type: text/html; charset=UTF-8');
-		# http::head(403,'Forbidden');
-		http::head(410,'Enocint catwaze moussi a sot');
-		dcCore::app()->url->type = '403';
-		echo dcCore::app()->tpl->getData('403.html');
-		exit;
-	}
-}
 ?>
